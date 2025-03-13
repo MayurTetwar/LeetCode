f760b9d1ecceb51320a299d2c88e6ae7828b31d4
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-03-13 15:12:24 +0530

    Time: 1230 ms (5.14%) | Memory: 60.2 MB (20.85%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -3,14 +3,9 @@ class Solution {
         int[] ans=new int[n];
         int m=arr.length;
         for(int i=0;i<m;i++){
-            int val=arr[i][2];
-            ans[arr[i][0]-1]+=val;
-            if(arr[i][1]<n){
-                ans[arr[i][1]]-=val;
+            for(int j=arr[i][0]-1;j<arr[i][1];j++){
+                ans[j]+=arr[i][2];
             }
-        }
-        for(int i=1;i<n;i++){
-            ans[i]+=ans[i-1];
         }
         return ans;
     }
