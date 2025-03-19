59b14b5d44a3a87fdc66a45e8d2db6667ba423fc
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-03-19 17:18:01 +0530

    Time: 15 ms (87.12%) | Memory: 55.9 MB (69.76%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long repairCars(int[] arr, int cars) {
+        long end=0;
+        for(int i=0;i<arr.length;i++){
+            end=Math.max(end,(long)arr[i]);
+        }
+        long st=0;
+        end=end*cars*cars;
+        while(st<end){
+            long mid=st+(end-st)/2;
+            //System.out.println(mid+" "+st+" "+end);
+            if(helper(mid,arr,cars)){
+                end=mid;
+            }else{
+                st=mid+1;
+            }
+        }
+        return end;
+    }
+    public boolean helper(long mid,int[] arr,int cars){
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            count+=(int)Math.sqrt((mid/(long)arr[i]));
+            if(count>=cars)return true;
+        }
+        return false;
+    }
+}
