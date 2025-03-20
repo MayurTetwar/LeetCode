5906f59499c3010cf407ea7953e9b8ec8a2dc198
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-03-21 00:09:16 +0530

    Time: 31 ms (19.41%) | Memory: 120.8 MB (45.29%) - LeetSync

---
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] minimumCost(int n, int[][] arr, int[][] que) {
+        List<List<int[]>> list=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(new ArrayList<>());
+        }
+        for(int []a:arr){
+            list.get(a[0]).add(new int[]{a[1],a[2]});
+            list.get(a[1]).add(new int[]{a[0],a[2]});
+        }
+        List<Integer> components=new ArrayList<>();
+        int[] com=new int[n];
+        int id=0;
+        boolean[] visi=new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visi[i]){
+                components.add(helper(i,list,com,id,visi));
+                id++;
+            }
+        }
+        int[] ans=new int[que.length];
+        for(int i=0;i<que.length;i++){
+            if(com[que[i][0]]==com[que[i][1]]){
+                ans[i]=components.get(com[que[i][0]]);
+            }else{
+                ans[i]=-1;
+            }
+        }
+        return ans;
+    }
+    public int helper(int i,List<List<int[]>> list,int[] com,int id,boolean[] visi){
+        visi[i]=true;
+        com[i]=id;
+        int min=Integer.MAX_VALUE;
+        for(int []a:list.get(i)){
+            min&=a[1];
+            if(!visi[a[0]]){
+                min&=helper(a[0],list,com,id,visi);
+            }
+        }
+        return min;
+    }
+}
