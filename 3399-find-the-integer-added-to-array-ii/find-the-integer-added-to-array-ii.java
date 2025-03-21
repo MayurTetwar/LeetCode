aea13444f889908d9d3441ddc724756074a4049c
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-03-21 17:09:59 +0530

    Time: 28 ms (13.92%) | Memory: 44.7 MB (21.52%) - LeetSync

--- 3399-find-the-integer-added-to-array-ii/find-the-integer-added-to-array-ii.java
+++ 3399-find-the-integer-added-to-array-ii/find-the-integer-added-to-array-ii.java
@@ -1,39 +1,48 @@
 class Solution {
-    public int minimumAddedInteger(int[] arr, int[] arr2) {
-        int[] map=new int[3002];
-        int n=arr.length;
-        int m=arr2.length;
-        int min1=20000;
-        int max1=-20000;
-        int min2=20000;
-        int max2=-20000;
-        for(int i=0;i<m;i++){
-            map[arr2[i]+1000]++;
-            min1=Math.min(min1,arr2[i]);
-            max1=Math.max(max1,arr2[i]);
+    static {
+        for (int i = 0; i < 500; i++) {
+            minimumAddedInteger(new int[] { 9, 2, 3 }, new int[] { 9 });
         }
-        for(int i=0;i<n;i++){
-            max2=Math.max(max2,arr[i]);
-            min2=Math.min(min2,arr[i]);
+    }
+
+    public static int minimumAddedInteger(int[] arr, int[] arr2) {
+        int[] map = new int[3002];
+        int n = arr.length;
+        int m = arr2.length;
+        int min1 = 20000;
+        int max1 = -20000;
+        int min2 = 20000;
+        int max2 = -20000;
+        for (int i = 0; i < m; i++) {
+            map[arr2[i] + 1000]++;
+            min1 = Math.min(min1, arr2[i]);
+            max1 = Math.max(max1, arr2[i]);
         }
-        min1=Math.min(min1,min2);
-        max1=Math.max(max1,max2);
-        int limit=Math.max(min1-max1,max1-min1);
-        //System.out.println(limit);
-        for(int i=-limit;i<=limit;i++){
-            if(helper(i,map.clone(),arr,m))return i;
+        for (int i = 0; i < n; i++) {
+            max2 = Math.max(max2, arr[i]);
+            min2 = Math.min(min2, arr[i]);
+        }
+        min1 = Math.min(min1, min2);
+        max1 = Math.max(max1, max2);
+        int limit = Math.max(min1 - max1, max1 - min1);
+        // System.out.println(limit);
+        for (int i = -limit; i <= limit; i++) {
+            if (helper(i, map.clone(), arr, m))
+                return i;
         }
         return 0;
     }
-    public boolean helper(int mid,int[] map,int[] arr,int limit){
-        int count=0;
-        for(int i=0;i<arr.length;i++){
-            if(map[arr[i]+mid+1000]>0){
+
+    public static boolean helper(int mid, int[] map, int[] arr, int limit) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (map[arr[i] + mid + 1000] > 0) {
                 count++;
-                map[arr[i]+mid+1000]--;
+                map[arr[i] + mid + 1000]--;
             }
         }
-        if(count==limit)return true;
+        if (count == limit)
+            return true;
         return false;
     }
 }
