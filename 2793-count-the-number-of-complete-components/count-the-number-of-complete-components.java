8e5e8985fbea19f92ea8d88c93ce1353334c10d8
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-03-22 15:09:11 +0530

    Time: 11 ms (57.88%) | Memory: 45.2 MB (72.09%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int countCompleteComponents(int total, int[][] arr) {
+        List<List<Integer>> list=new ArrayList<>();
+        int n=arr.length;
+        for(int i=0;i<total;i++){
+            list.add(new ArrayList<>());
+        }
+        for(int []a:arr){
+            list.get(a[0]).add(a[1]);
+            list.get(a[1]).add(a[0]);
+        }
+        int count=0;
+        boolean[] visi=new boolean[total];
+
+        for(int i=0;i<total;i++){
+            if(!visi[i]){
+                List<Integer> all=new ArrayList<>();
+                int size=helper(i,list,visi,total,all);
+                int j=0;
+                for(j=0;j<all.size();j++){
+              //  System.out.println(list.get(all.get(j)).size()+" "+(size-1));
+                    if(list.get(all.get(j)).size()!=size-1)break;
+                }
+                if(j==all.size())count++;
+            }
+        }
+        return count;
+    }
+    public int helper(int node,List<List<Integer>> list,boolean[] visi,int total,List<Integer> all){
+        visi[node]=true;
+        all.add(node);
+        int count=1;
+        for(int num:list.get(node)){
+            if(!visi[num]){
+                //System.out.println(size+" "+list.get(num).size)  
+                count+=helper(num,list,visi,total,all);
+            }
+        }
+        return count;
+    }
+}
