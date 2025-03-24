ea4c9eb8c3dc3dfd0add0279e75052b3088c0a4d
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-03-24 15:49:05 +0530

    Time: 58 ms (66.03%) | Memory: 42.7 MB (85.44%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,27 @@
+class Solution {
+    // static{
+    //     for(int i=0;i<500;i++){
+    //         beautySum("a");
+    //     }
+    // }
+    public static int beautySum(String s) {
+        int count=0;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            int[] map=new int[26];
+            for(int j=i;j<n;j++){
+                map[s.charAt(j)-'a']++;
+                int min=Integer.MAX_VALUE;
+                int max=Integer.MIN_VALUE;
+                for(int k=0;k<26;k++){
+                    if(map[k]>0){
+                        max=Math.max(max,map[k]);
+                        min=Math.min(min,map[k]);
+                    }
+                }
+                count+=(max-min);
+            }
+        }
+        return count;
+    }
+}
