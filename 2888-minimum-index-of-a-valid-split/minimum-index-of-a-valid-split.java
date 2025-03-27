7ee14ae2c51c12817897a733b61b9a0a26d070ac
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-03-27 07:33:51 +0530

    Time: 34 ms (53.85%) | Memory: 66.8 MB (5.77%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minimumIndex(List<Integer> list) {
+        HashMap<Integer,List<Integer>> map=new HashMap<>();
+        int n=list.size();
+        for(int i=0;i<n;i++){
+            int val=list.get(i);
+            if(map.containsKey(val)){
+                List<Integer> l=map.get(val);
+                l.add(i);
+                map.put(val,l);
+            }else{
+                List<Integer> l=new ArrayList<>();
+                l.add(i);
+                map.put(val,l);
+            }
+        }
+        int max=0;
+        List<Integer> maxList=new ArrayList<>();
+        for(int num:map.keySet()){
+            if(map.get(num).size()*2>n){
+                max=num;
+                maxList=map.get(num);
+                break;
+            }
+        }
+        int frq=1;
+        int m=maxList.size();
+        for(int i=0;i<m-1;i++){
+            if(frq*2>maxList.get(i)+1){
+                if((m-frq)*2>n-maxList.get(i)-1){
+                    return maxList.get(i);
+                }
+            }else if(frq*2>maxList.get(i+1)){
+                if((m*frq)*2>n-maxList.get(i+1)){
+                    return maxList.get(i+1);
+                }
+            }
+            frq++;
+        }
+        return -1;
+    }
+}
