98ed358150dbf5c4133b7cfe3fe1a3c081da45c9
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-03-28 08:56:04 +0530

    Time: 103 ms (45.04%) | Memory: 62.2 MB (9.92%) - LeetSync

---
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.java
@@ -0,0 +1,59 @@
+class Solution {
+    class Cell{
+        int i, j, val;
+        Cell(int i, int j, int val){
+            this.i=i;
+            this.j=j;
+            this.val=val;
+        }
+    }
+    public int[] maxPoints(int[][] arr, int[] qua) {
+        
+        int k=qua.length;
+        int n=arr.length;
+        int m=arr[0].length;
+        PriorityQueue<Cell> q=new PriorityQueue<>(Comparator.comparingInt(cell->cell.val));
+        boolean[][] visi=new boolean[n][m];
+        int[][] dir={{0,1},{1,0},{0,-1},{-1,0}};
+        //int[] ans=new int[k];
+        int max=0;
+        for(int i=0;i<k;i++){
+            max=Math.max(max,qua[i]);
+        }
+        int[] copy=qua.clone();
+        int[] res =new int[max+1];
+        Arrays.sort(qua);
+        q.add(new Cell(0,0,arr[0][0]));
+            visi[0][0]=true;
+            int point=0;
+
+        for(int i=0;i<k;i++){
+            int num=qua[i];
+            
+            while(q.size()>0 && q.peek().val<num){
+                Cell curr=q.poll();
+                point++;
+                    for(int[] d:dir){
+                        int newi=d[0]+curr.i;
+                        int newj=d[1]+curr.j;
+                        if(isvalid(n,m,newi,newj) && !visi[newi][newj]){
+                            
+                            //System.out.print(ans+" "+arr[newi][newj]);
+                            q.add(new Cell(newi,newj,arr[newi][newj]));
+                            visi[newi][newj]=true;
+                        }
+                    }
+                    //System.out.println();
+            }
+            res[num]=point;
+        }
+        for(int i=0;i<k;i++){
+            qua[i]=res[copy[i]];
+        }
+        return qua;
+    }
+    public boolean isvalid(int n ,int m ,int i, int j){
+        if(i>=n || j>=m || i<0 || j<0)return false;
+        return true;
+    }
+}
