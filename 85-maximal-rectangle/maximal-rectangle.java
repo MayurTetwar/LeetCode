327ad25e78de3699cc4b297ab244e4b475c824e5
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-03-29 15:23:31 +0530

    Time: 32 ms (6.76%) | Memory: 45.4 MB (89.57%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int maximalRectangle(char[][] arr) {
+        int n=arr.length;
+        int m=arr[0].length;
+        int[] dp=new int[m];
+        Arrays.fill(dp,1);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int[] left=new int[m];
+            int[] right=new int[m];
+            Stack<Integer> stk=new Stack<>();
+            for(int j=0;j<m;j++){
+                if((arr[i][j]-'0')==1)dp[j]++;
+                else dp[j]=1;
+                while(!stk.isEmpty() && dp[stk.peek()]>=dp[j]){
+                    stk.pop();
+                }
+                if(stk.size()>0)left[j]=stk.peek();
+                else left[j]=-1;
+                stk.push(j);
+            }
+           // System.out.println(Arrays.toString(dp));
+            stk=new Stack<>();
+            for(int j=m-1;j>=0;j--){
+                while(!stk.isEmpty() && dp[stk.peek()]>=dp[j]){
+                    stk.pop();
+                }
+                if(stk.size()>0)right[j]=stk.peek();
+                else right[j]=m;
+                stk.push(j);
+            }
+            for(int j=0;j<m;j++){
+                ans=Math.max(ans,(right[j]-left[j]-1)*(dp[j]-1));
+            }
+        }
+        return ans;
+    }
+}
