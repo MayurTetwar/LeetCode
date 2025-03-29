ecae63c91b800553300a1918977b8224cf807049
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-03-29 15:53:09 +0530

    Time: 14 ms (81.18%) | Memory: 46.8 MB (39.17%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -3,35 +3,27 @@ class Solution {
         int n=arr.length;
         int m=arr[0].length;
         int[] dp=new int[m];
-        Arrays.fill(dp,1);
         int ans=0;
         for(int i=0;i<n;i++){
-            int[] left=new int[m];
-            int[] right=new int[m];
-            Stack<Integer> stk=new Stack<>();
             for(int j=0;j<m;j++){
                 if((arr[i][j]-'0')==1)dp[j]++;
-                else dp[j]=1;
-                while(!stk.isEmpty() && dp[stk.peek()]>=dp[j]){
-                    stk.pop();
-                }
-                if(stk.size()>0)left[j]=stk.peek();
-                else left[j]=-1;
-                stk.push(j);
+                else dp[j]=0;
             }
-           // System.out.println(Arrays.toString(dp));
-            stk=new Stack<>();
-            for(int j=m-1;j>=0;j--){
-                while(!stk.isEmpty() && dp[stk.peek()]>=dp[j]){
-                    stk.pop();
-                }
-                if(stk.size()>0) ans=Math.max(ans,(stk.peek()-left[j]-1)*(dp[j]-1));
-                else ans=Math.max(ans,(m-left[j]-1)*(dp[j]-1));
-                stk.push(j);
+            ans=Math.max(ans,helper(dp));
+        }
+        return ans;
+    }
+    public int helper(int[] arr){
+        Stack<Integer> stk=new Stack<>();
+        int n=arr.length;
+        int ans=0;
+        for(int i=0;i<=n;i++){
+            while(!stk.isEmpty() && (i==n || arr[stk.peek()]>=arr[i])){
+                int hight=arr[stk.pop()];
+                int width=(stk.isEmpty())?i:i-stk.peek()-1;
+                ans=Math.max(ans,hight*width);
             }
-            // for(int j=0;j<m;j++){
-            //     ans=Math.max(ans,(right[j]-left[j]-1)*(dp[j]-1));
-            // }
+            stk.push(i);
         }
         return ans;
     }
