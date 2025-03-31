8fbf0fc07a5acdc734f7f44888ae34fe1e9a3a72
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-03-31 09:31:33 +0530

    Time: 31 ms (100.00%) | Memory: 62 MB (68.66%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.java
@@ -1,11 +1,16 @@
 class Solution {
-    public long putMarbles(int[] arr, int k) {
+    static{
+        for(int i=0;i<500;i++){
+            putMarbles(new int[]{1,2},1);
+        }
+    }
+    public static long putMarbles(int[] arr, int k) {
         int n=arr.length;
         long[] temp=new long[n-1];
         for(int i=0;i<n-1;i++){
             temp[i]=arr[i]+arr[i+1];
         }
-        Arrays.sort(temp);
+        Arrays.sort(temp,0,n-1);
         long ans=0;
         for(int i=0;i<k-1;i++){
             ans+=(temp[n-2-i]-temp[i]);
