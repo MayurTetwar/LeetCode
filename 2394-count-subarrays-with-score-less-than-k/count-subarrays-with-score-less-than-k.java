8fb23b772152ed7829f94f78a5aac641796ffe81
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-04-28 12:30:24 +0530

    Time: 49 ms (5.22%) | Memory: 57.4 MB (98.31%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.java
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.java
@@ -1,5 +1,10 @@
 class Solution {
-    public long countSubarrays(int[] arr, long k) {
+    static{
+        for(int i=0;i<500;i++){
+            countSubarrays(new int[]{1},0);
+        }
+    }
+    public static long countSubarrays(int[] arr, long k) {
         int n = arr.length;
         long prefix[] = new long[n];
         prefix[0] = arr[0];
@@ -10,43 +15,25 @@ class Solution {
         long pre = 0;
         int i = 0;
         int ind = 0;
-        int save = 0;
         for (i = 0; i < n; i++) {
             ind = helper(prefix, k, pre, i) - i;
-            //System.out.println(save+" "+ind+" "+i+" "+ans);
-            // if(ind>save)ans+=(poss(ind)-poss(i-save));
             ans += ind;
             pre = prefix[i];
-            // save=ind-1;
-            // if(ind<=i)i++;
-            // else i=ind;
         }
-        // if(i<n){
-        //     //System.out.println("Hi");
-        //     if(arr[n-1]<k)ans++;
-        // }
         return ans;
     }
 
-    public long poss(int num) {
-        //System.out.println(((num+1)*num)/2);
-        return ((num + 1) * num) / 2;
-    }
-
-    public int helper(long[] arr, long k, long pre, int i) {
-        // //System.out.println(i+" "+pre);
+    public static int helper(long[] arr, long k, long pre, int i) {
         int st = i;
         int end = arr.length;
         while (st < end) {
             int mid = (end + st) / 2;
-            // //System.out.println(end+" "+st+" "+(arr[mid]-pre)+" "+(mid-i+1));
             if (((arr[mid] - pre) * (mid - i + 1)) < k) {
                 st = mid + 1;
             } else {
                 end = mid;
             }
         }
-        // if(end==arr.length-1)return end+1;
         return end;
     }
 }
