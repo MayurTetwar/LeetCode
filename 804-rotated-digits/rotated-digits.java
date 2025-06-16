d4e1e0c7da32bbdb2a84c48719a37300df3b2d94
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-06-16 12:44:27 +0530

    Time: 3 ms (95.76%) | Memory: 41.1 MB (35.66%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,17 +1,26 @@
 class Solution {
-    public int rotatedDigits(int n) {
+    static int[] ans=new int[10001];
+    static{
+        getOrans(10000);
+    }
+    static public int getOrans(int n){
         int count=0;
+       // System.out.println(n);
         for(int i=1;i<=n;i++){
             String s=String.valueOf(i);
             if(conOne(s) && conTwo(s))count++;
+            ans[i]=count;
         }
         return count;
     }
-    public boolean conOne(String s){
+    static public int rotatedDigits(int n) {
+        return ans[n];
+    }
+    static public boolean conOne(String s){
         if(s.contains("2") || s.contains("5") || s.contains("6") || s.contains("9"))return true;
         return false;
     }
-    public boolean conTwo(String s){
+    static public boolean conTwo(String s){
         if(s.contains("3") || s.contains("4") || s.contains("7"))return false;
         return true;
     }
