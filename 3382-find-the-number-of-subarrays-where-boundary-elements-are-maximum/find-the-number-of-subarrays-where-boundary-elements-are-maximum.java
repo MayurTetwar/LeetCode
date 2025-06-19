f79009c60cee600f2968fd455867aef047d60bf6
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-06-19 13:01:59 +0530

    Time: 66 ms (57.65%) | Memory: 57.1 MB (94.71%) - LeetSync

--- 3382-find-the-number-of-subarrays-where-boundary-elements-are-maximum/find-the-number-of-subarrays-where-boundary-elements-are-maximum.java
+++ 3382-find-the-number-of-subarrays-where-boundary-elements-are-maximum/find-the-number-of-subarrays-where-boundary-elements-are-maximum.java
@@ -2,46 +2,21 @@ class Solution {
     public long numberOfSubarrays(int[] arr) {
         long ans=0;
         int n=arr.length;
-        PriorityQueue<long[]> pq=new PriorityQueue<>((a,b)->{
-            if(a[0]>b[0])return 1;
-            return -1;
-        });
+        Stack<long[]> stk=new Stack<>();
 
         for(int i=0;i<n;i++){
-           // if(pq.size()>0)System.out.println(i+" "+pq.peek()[0]+" "+arr[i]);
-            while(pq.size()>0 && pq.peek()[0]<arr[i]){
-                pq.poll();
+            while(stk.size()>0 && stk.peek()[0]<arr[i]){
+                stk.pop();
             }
-            if(pq.size()>0 && pq.peek()[0]==arr[i]){
-                long max[]=pq.poll();
+            if(stk.size()>0 && stk.peek()[0]==arr[i]){
+                long max[]=stk.pop();
                 ans+=max[1];
                 max[1]++;
-                pq.add(max);
+                stk.push(max);
             }else{
-                pq.add(new long[]{(long)arr[i],1L});
+                stk.push(new long[]{(long)arr[i],1L});
             }
         }
-        // long ans=0;
-        // int max=-1;
-        // int n=arr.length;
-        // Map<Integer,Long> map=new HashMap<>();
-        // for(int i=0;i<n;i++){
-        //     if(max<arr[i]){
-        //         map=new HashMap<>();
-        //         max=arr[i];
-        //         map.put(arr[i],1L);
-        //     }else{
-        //         if(map.containsKey(arr[i])){
-        //             long val=map.get(arr[i]);
-        //             ans+=val;
-        //             if(arr[i]==max)map=new HashMap<>();
-        //             map.put(arr[i],val);
-        //         }
-                
-        //         map.put(arr[i],map.getOrDefault(arr[i],0L)+1L);
-        //     }
-        //     System.out.println(map+" "+i+" "+ans);
-        // }
         return ans+n;
     }
 }
