d2e6cf2de2c80a54b251b1dbfd9b57c94787003b
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-06-22 14:27:52 +0530

    Time: 96 ms (56.02%) | Memory: 41.2 MB (37.10%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.java
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.java
@@ -2,21 +2,18 @@ class Solution {
     int ans;
     public int distributeCookies(int[] arr, int k) {
         ans=Integer.MAX_VALUE;
-        helper(arr,new int[k],0);
+        helper(arr,new int[k],0,0);
         return ans;
     }
-    public void helper(int[] arr,int[] arrk,int j){
+    public void helper(int[] arr,int[] arrk,int j,int max){
+        if(max>ans)return;
         if(j==arr.length){
-            int max=0;
-            for(int i=0;i<arrk.length;i++){
-                max=Math.max(max,arrk[i]);
-            }
             ans=Math.min(ans,max);
             return ;
         }
         for(int i=0;i<arrk.length;i++){
             arrk[i]+=arr[j];
-            helper(arr,arrk,j+1);
+            helper(arr,arrk,j+1,Math.max(max,arrk[i]));
             arrk[i]-=arr[j];
         }
     }
