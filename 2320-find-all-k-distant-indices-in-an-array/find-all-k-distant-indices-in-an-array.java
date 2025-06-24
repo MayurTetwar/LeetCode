9c9b75af611ca6b4ace2c6c7dfa6d728b1c3d850
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-06-24 06:54:37 +0530

    Time: 89 ms (27.82%) | Memory: 45.4 MB (28.10%) - LeetSync

---
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> findKDistantIndices(int[] arr, int key, int k) {
+
+        int n=arr.length;
+        List<Integer> keyInd = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(key==arr[i])keyInd.add(i);
+        }
+        HashSet<Integer> set=new HashSet<>();
+        for(int ind:keyInd){
+            set.add(ind);
+            int k2=k;
+            int k3=k;
+            for(int i=ind-1;i>=0 && k3-->0;i--){
+                set.add(i);
+            }
+            for(int i=ind+1;i<n && k2-->0;i++){
+                set.add(i);
+            }
+        }
+        List<Integer> ans=new ArrayList<>();
+        for(int num:set){
+            ans.add(num);
+        }
+        Collections.sort(ans);
+        return ans;
+    }
+}
