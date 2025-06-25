0bc848e8f5830666f1a9ee8ba1bb9131a34f2780
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-06-25 17:02:21 +0530

    Time: 68 ms (79.63%) | Memory: 45.7 MB (64.81%) - LeetSync

---
+++ 2241-recover-the-original-array/recover-the-original-array.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] recoverArray(int[] arr) {
+        int n=arr.length;
+        Arrays.sort(arr);
+        for(int i=1;i<n;i++){
+            int[] res=new int[n/2];
+            if(helper(arr,(arr[i]-arr[0])/2,res)){
+                return res;
+            }
+        }
+        return arr;
+    }
+    public boolean helper(int[] arr,int k,int[] res){
+        int i=0,j=1;
+        System.out.println(k);
+        int n=arr.length;
+        int ind=0;
+        boolean[] visi=new boolean[n];
+        while(j<n){
+            if(visi[i]){
+                i++;
+                continue;
+            }
+            if(visi[j]){
+                j++;
+                continue;
+            }
+            int diff=arr[j]-arr[i];
+            if(diff>2*k){
+                i++;
+            }else if(diff<2*k){
+                j++;
+            }else{
+                visi[i]=true;
+                visi[j]=true;
+                if(ind==res.length)return false;
+                res[ind++]=arr[i]+k;
+                i++;
+                j++;
+            }
+        }
+        if(ind==res.length)return true;
+        return false;
+    } 
+}
