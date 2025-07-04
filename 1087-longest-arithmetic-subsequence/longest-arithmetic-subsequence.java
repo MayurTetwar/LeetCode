9980bc7cd9aab09c227dd2568a3c4e93e0d0311a
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-07-04 12:43:40 +0530

    Time: 50 ms (84.01%) | Memory: 55.5 MB (87.18%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
@@ -1,28 +1,19 @@
 class Solution {
-    int[][] memo;
     public int longestArithSeqLength(int[] arr) {
-        int ans=0;
         int n=arr.length;
-        memo=new int[n][1001];
+        int[][] dp=new int[n][1001];
         for(int i=0;i<n;i++){
-            Arrays.fill(memo[i],1000);
+            Arrays.fill(dp[i],1);
         }
-        for(int i=0;i<n;i++){
-            ans=Math.max(ans,helper(arr,i,1000));
+        int max=0;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                int diff=arr[i]-arr[j];
+                int offset=diff+500;
+                dp[i][offset]=Math.max(dp[i][offset],dp[j][offset]+1);
+                max=Math.max(max,dp[i][offset]);
+            }
         }
-        return Math.max(2,ans);
-    }
-    public int helper(int[] arr,int i,int diff){
-        if(i==arr.length){
-            return 0;
-        }
-        if(diff!=1000 && memo[i][diff+500]!=1000)return memo[i][diff+500];
-        int ans=1;
-        for(int j=i+1;j<arr.length;j++){
-            if(diff==1000 || diff==arr[j]-arr[i])
-                ans=Math.max(ans,1+helper(arr,j,arr[j]-arr[i]));
-        }
-        if(diff!=1000)memo[i][diff+500]=ans;
-        return ans;
+        return max;
     }
 }
