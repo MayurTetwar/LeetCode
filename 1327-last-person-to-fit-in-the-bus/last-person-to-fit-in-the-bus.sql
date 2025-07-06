5334e1e740aa40205b0838bb070566d04ccb1f65
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-07-06 20:15:52 +0530

    Time: 417 ms (91.49%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,15 +1,13 @@
 -- Write your PostgreSQL query statement below
--- select person_name
--- from (
-    select 
-    q1.person_name
-    from Queue q1 join Queue q2
-    on q1.turn>=q2.turn
-    group by q1.turn,q1.person_name
-    having sum(q2.weight)<=1000
-    order by sum(q2.weight) desc
-    limit 1
--- )
--- order by sum desc
--- limit 1;
+select person_name
+from (
+    select q1.person_name,
+    sum(q1.weight) over (order by q1.turn)
+    from Queue q1
+)
+where sum<=1000
+order by sum desc
+limit 1
+
+
 
