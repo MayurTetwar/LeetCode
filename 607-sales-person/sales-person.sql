45cd5c2e5fa65b8cde3ce6985bbfa385bcba8240
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-07-10 23:21:44 +0530

    Time: 332 ms (61.52%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,12 +1,10 @@
 -- Write your PostgreSQL query statement below
 select sp.name
-from SalesPerson sp left join Orders o
-on sp.sales_id = o.sales_id        
-left join Company com 
-on o.com_id = com.com_id
-group by sp.name
-having 0=count(
-    case
-        when com.name='RED' then 1
-    end
-);
+from SalesPerson sp 
+where sp.sales_id not in(
+    select sales_id 
+    from Orders o left join
+    Company c
+    on o.com_id=c.com_id
+    where c.name='RED'
+)
