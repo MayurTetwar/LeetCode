3dc0e2b4a3e69cc356d0e36004f96da40e9ace30
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-07-11 00:00:47 +0530

    Time: 1402 ms (5.14%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,10 +1,11 @@
 -- Write your PostgreSQL query statement below
-select product_id,product_name 
-from Product
-where product_id not in (
-    select s.product_id 
-    from Sales s
-    where s.sale_date not between '2019-01-01' and '2019-03-31'
-) and product_id in (
-    select s.product_id from sales s
-)
+select p.product_id,p.product_name 
+from Product p join Sales s
+ON p.product_id = s.product_id
+where 
+p.product_id not in (
+    select product_id 
+    from Sales
+    where sale_date<'2019-01-01' or sale_date>'2019-03-31'
+)  
+GROUP BY p.product_id, p.product_name
