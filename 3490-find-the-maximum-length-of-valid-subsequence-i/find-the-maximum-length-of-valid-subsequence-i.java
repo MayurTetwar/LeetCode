9e39a6d5bb3855bdd789b8e7c4d650f21fe0cb95
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-07-16 10:29:21 +0530

    Time: 12 ms (22.13%) | Memory: 62.2 MB (74.59%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.java
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.java
@@ -1,53 +1,45 @@
 class Solution {
-    int[][] memo;
     public int maximumLength(int[] arr) {
-        memo=new int[arr.length][2];
-        for(int i=0;i<arr.length;i++){
-            memo[i][0]=-1;
-            memo[i][1]=-1;
+        int ev=0;
+        int odd=0;
+        int ans=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0)ev++;
+            else odd++;
         }
-        int len1=helperOne(arr,0,true);
-        int len2=helperOne(arr,0,false);
-        memo=new int[arr.length][2];
-        for(int i=0;i<arr.length;i++){
-            memo[i][0]=-1;
-            memo[i][1]=-1;
-        }
-        int len3=helperZero(arr,0,true);
-        int len4=helperZero(arr,0,false);
-        return Math.max(Math.max(len1,len2),Math.max(len3,len4));
-    }
-    
-    public int helperZero(int[] arr,int i,boolean odd){
-        if(i==arr.length)return 0;
-        if(memo[i][(odd)?0:1]!=-1)return memo[i][(odd)?0:1]; 
-        int len1=helperZero(arr,i+1,odd);
-        int len2=0;
-        if(odd){
-            if(arr[i]%2!=0){
-                len2=helperZero(arr,i+1,odd)+1;
-            }
-        }else{
-             if(arr[i]%2==0){
-                len2=helperZero(arr,i+1,odd)+1;
+        ans=Math.max(ev,odd);
+        int ans2=0;
+        boolean even=true;
+        for(int i=0;i<n;i++){
+            if(even){
+                if(arr[i]%2!=0){
+                    ans2++;
+                    even=false;
+                }
+            }else{
+                if(arr[i]%2==0){
+                    ans2++;
+                    even=true;
+                }
             }
         }
-        return memo[i][(odd)?0:1]=Math.max(len1,len2);
-    }
-    public int helperOne(int[] arr,int i,boolean odd){
-        if(i==arr.length)return 0;
-        if(memo[i][(odd)?0:1]!=-1)return memo[i][(odd)?0:1];
-        int len1=helperOne(arr,i+1,odd);
-        int len2=0;
-        if(odd){
-            if(arr[i]%2==0){
-                len2=helperOne(arr,i+1,false)+1;
-            }
-        }else{
-             if(arr[i]%2!=0){
-                len2=helperOne(arr,i+1,true)+1;
+        ans=Math.max(ans,ans2);
+        ans2=0;
+        even=false;
+        for(int i=0;i<n;i++){
+            if(even){
+                if(arr[i]%2!=0){
+                    ans2++;
+                    even=false;
+                }
+            }else{
+                if(arr[i]%2==0){
+                    ans2++;
+                    even=true;
+                }
             }
         }
-        return memo[i][(odd)?0:1]=Math.max(len1,len2);
+        return Math.max(ans,ans2);
     }
 }
