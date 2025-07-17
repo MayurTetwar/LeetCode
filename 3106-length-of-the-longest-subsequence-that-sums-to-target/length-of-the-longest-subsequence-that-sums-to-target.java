6a2b45726cdbb3845008c449122e9197a9a4b0b7
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-07-17 11:06:22 +0530

    Time: 287 ms (50.39%) | Memory: 55.4 MB (30.58%) - LeetSync

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.java
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.java
@@ -1,23 +1,32 @@
 class Solution {
     int ninf=Integer.MIN_VALUE;
-    int[][] memo;
+    int[][] dp;
     public int lengthOfLongestSubsequence(List<Integer> list, int target) {
-        memo=new int[list.size()][target+1];
-        for(int i=0;i<list.size();i++){
-            Arrays.fill(memo[i],-1);
+        int n=list.size();
+        dp=new int[n+1][target+1];
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dp[i],-1);
+            dp[i][0]=0;
         }
-        int len=helper(list,0,target);
-        return (len<=0)?-1:len;
-    }
-    public int helper(List<Integer> list,int i,int tar){
-        if(tar==0)return 0;
-        if(list.size()==i)return ninf;
-        if(memo[i][tar]!=-1)return memo[i][tar];
-        int len1=helper(list,i+1,tar);
-        int len2=ninf;
-        if(tar>=list.get(i)){
-            len2=1+helper(list,i+1,tar-list.get(i));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                dp[i][j]=dp[i-1][j];
+                if(j>=list.get(i-1) && dp[i-1][j-list.get(i-1)]!=-1){
+                    dp[i][j]=Math.max(1+dp[i-1][j-list.get(i-1)],dp[i][j]);
+                }
+            }
         }
-        return memo[i][tar]=Math.max(len1,len2);
+        return dp[n][target];
     }
+    // public int helper(List<Integer> list,int i,int tar){
+    //     if(tar==0)return 0;
+    //     if(list.size()==i)return ninf;
+    //     if(memo[i][tar]!=-1)return memo[i][tar];
+    //     int len1=helper(list,i+1,tar);
+    //     int len2=ninf;
+    //     if(tar>=list.get(i)){
+    //         len2=1+helper(list,i+1,tar-list.get(i));
+    //     }
+    //     return memo[i][tar]=Math.max(len1,len2);
+    // }
 }
