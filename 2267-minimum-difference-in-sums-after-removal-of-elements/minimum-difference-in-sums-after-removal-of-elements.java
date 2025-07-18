308ed998bf4c5c556635d14e12b4d294485668b0
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-07-18 09:32:45 +0530

    Time: 139 ms (86.23%) | Memory: 78.8 MB (80.43%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.java
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.java
@@ -1,77 +1,43 @@
 class Solution {
     public long minimumDifference(int[] arr) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->{
-            if(a>b)return -1;
-            return 1;
-        });
         int n=arr.length;
-        long[] pre=new long[n];
-        long min=0;
-        for(int i=0;i<n/3;i++){
-            pq.add(arr[i]);
-            min+=arr[i];
-            pre[i]=min;
+        PriorityQueue<Integer> pqr=new PriorityQueue<>(Comparator.reverseOrder());
+        int parts=n/3;
+        long pre[]=new long[n];
+        long sumA=0;
+        for(int i=0;i<parts;i++){
+            pqr.add(arr[i]);
+            sumA+=arr[i];
+            pre[i]=sumA;
         }
-        for(int i=n/3;i<2*n/3;i++){
-            if(min-pq.peek()+arr[i]<min){
-                min=min-pq.poll()+arr[i];
+        for(int i=parts;i<2*parts;i++){
+            if(sumA>sumA-pqr.peek()+arr[i]){
+                sumA=sumA-pqr.poll()+arr[i];
+                pqr.add(arr[i]);
+            }
+            pre[i]=sumA;
+        }
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        long suff[]=new long[n];
+        long sumB=0;
+        for(int i=n-1;i>=2*parts;i--){
+            pq.add(arr[i]);
+            sumB+=arr[i];
+            suff[i]=sumB;
+        }
+        for(int i=2*parts-1;i>=parts;i--){
+            if(sumB<sumB-pq.peek()+arr[i]){
+                sumB=sumB-pq.poll()+arr[i];
                 pq.add(arr[i]);
             }
-            ////System.out.println(min);
-            pre[i]=min;
+            suff[i]=sumB;
         }
-      //  ////System.out.println(min+" "+Arrays.toString(visi));
-        pq=new PriorityQueue<>((a,b)->{
-            if(a>b)return 1;
-            return -1;
-        });
-        long[] suff=new long[n];
-        long max=0;
-        for(int i=n-1;i>=n*2/3;i--){
-            pq.add(arr[i]);
-            max+=arr[i];
-            suff[i]=max;
-        }
-        for(int i=(n*2/3)-1;i>=n/3;i--){
-            if(max-pq.peek()+arr[i]>max){
-                max=max-pq.poll()+arr[i];
-                pq.add(arr[i]);
-            }
-            suff[i]=max;
-        }
-        //System.out.println(Arrays.toString(pre));
-        //System.out.println(Arrays.toString(suff));
+        // System.out.println(Arrays.toString(pre));
+        // System.out.println(Arrays.toString(suff));
         long ans=Long.MAX_VALUE;
-        for(int i=n/3-1;i<(n*2/3);i++){
-            //System.out.println(pre[i]+" "+suff[i]);
+        for(int i=parts-1;i<2*parts;i++){
             ans=Math.min(ans,pre[i]-suff[i+1]);
         }
         return ans;
     }
 }
-/*
-class Solution {
-    public long minimumDifference(int[] arr) {
-        int n=arr.length;
-       
-        return helper(arr,0,0L,n/3,0L,n/3,n/3);
-    }
-    public long helper(int[] arr,int i,long sumA,int a,long sumB,int b,int c){
-        if(a==0 && b==0 && c==0){
-            //////System.out.println(sumA+" "+sumB);
-            return sumA-sumB;
-        }
-        if(i>=arr.length)return Long.MAX_VALUE;
-        long ans=Long.MAX_VALUE;
-        if(a>0){
-            ans=Math.min(ans,helper(arr,i+1,sumA+arr[i],a-1,sumB,b,c));
-        }
-        if(b>0){
-            ans=Math.min(ans,helper(arr,i+1,sumA,a,sumB+arr[i],b-1,c));
-        }
-        if(c>0)
-            ans=Math.min(ans,helper(arr,i+1,sumA,a,sumB,b,c-1));
-        return ans;
-    }
-}
-*/
