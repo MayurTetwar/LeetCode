6161c79202ec297a7123cbee5610d2707e52adee
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-07-19 10:21:47 +0530

    Time: 14 ms (6.71%) | Memory: 45.4 MB (6.71%) - LeetSync

--- 685-redundant-connection-ii/redundant-connection-ii.java
+++ 685-redundant-connection-ii/redundant-connection-ii.java
@@ -2,6 +2,7 @@ class Solution {
     int n;
     int[] par;
     int[] visi;
+    int com;
     public int find(int x){
         if(par[x]!=x){
             return find(par[x]);
@@ -13,30 +14,28 @@ class Solution {
         int com2=find(y);
         if(com1==com2)return ;
         par[com1]=com2;
+        com--;
     }
     public int[] findRedundantDirectedConnection(int[][] a) {
         int[] ans=new int[2];
         n=a.length;
+        com=a.length;
         for(int i=n-1;i>=0;i--){
             par=new int[n+1];
             for(int j=0;j<=n;j++)par[j]=j;
+            com=n;
             visi=new int[n+1];
+            boolean isValid=true;
             for(int j=0;j<n;j++){
                 if(i==j)continue;
                 union(a[j][0],a[j][1]);
                 visi[a[j][1]]++;
-            }
-            int j=1;
-            int parent=find(j);
-            if(visi[j]>1)continue;
-            // System.out.println(parent);
-            for(j=2;j<=n;j++){
-            // System.out.println(find(j));
-                if(parent!=find(j) || visi[j]>1){
+                if(visi[a[j][1]]>1){
+                    isValid=false;
                     break;
                 }
             }
-            if(j==n+1)return a[i];
+            if(com==1 && isValid)return a[i];
         }
         return ans;
     }
