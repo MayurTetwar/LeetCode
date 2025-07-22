00b7a9b3c0eaabd5774aad0fcb7fc9dbc4b656c1
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-07-22 18:56:10 +0530

    Time: 187 ms (75.25%) | Memory: 57.8 MB (46.48%) - LeetSync

--- 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.java
+++ 2339-k-divisible-elements-subarrays/k-divisible-elements-subarrays.java
@@ -3,33 +3,17 @@ class Solution {
         Set<String> set=new HashSet<>();
         int n=arr.length;
         int ans=0;
-        int[] count=new int[n];
-        if(arr[0]%p==0)count[0]++;
-        for(int i=1;i<n;i++){
-            if(arr[i]%p==0)count[i]++;
-            count[i]+=count[i-1];
-        }
-        //System.out.println(Arrays.toString(count));
         for(int i=0;i<n;i++){
+            int count=0;
             StringBuilder sb=new StringBuilder();
             for(int j=i;j<n;j++){
-                if (j>i) {
-                    sb.append(",");
-                }
-                sb.append(arr[j]);
-                if(set.contains(sb.toString()))continue;
+                if(arr[j]%p==0)count++;
+                if(count>k)break;
+                sb.append(arr[j]).append(",");
                 set.add(sb.toString());
-                if(i==0 && count[j]>k)break;
-                if(i>=1 && count[j]-count[i-1]>k)break;
-                ans++;
             }
-            //System.out.print(ans+" ");
-            // for(String s:set){
-            //     //System.out.print(s+"|");
-            // }
-            //System.out.println();
         }
-        return ans;
+        return set.size();
     }
     /*
     [2], [2,3], [2,3,3], [2,3,3,2], [3], [3,3], [3,3,2], [3,3,2,2], [3,2], [3,2,2], and [2,2]
