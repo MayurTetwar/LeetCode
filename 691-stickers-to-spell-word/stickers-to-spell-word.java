0fcc7a33e4cbe2884f4129d82f187cdeb5c5f64e
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-07-28 14:30:58 +0530

    Time: 269 ms (15.21%) | Memory: 45.7 MB (24.23%) - LeetSync

--- 691-stickers-to-spell-word/stickers-to-spell-word.java
+++ 691-stickers-to-spell-word/stickers-to-spell-word.java
@@ -1,59 +1,56 @@
 class Solution {
     HashMap<String,Integer> map;
     public int minStickers(String[] strArr, String tar) {
-        int[] tarc=new int[26];
-        int n=strArr.length;
-        int m=tar.length();
         map=new HashMap<>();
-        for(int i=0;i<m;i++){
-            tarc[tar.charAt(i)-'a']++;
-        }
+        int n=strArr.length;
         int[][] count=new int[n][26];
         for(int i=0;i<n;i++){
             for(int j=0;j<strArr[i].length();j++){
                 count[i][strArr[i].charAt(j)-'a']++;
             }
         }
-        int ans=helper(count,tarc,0);
+        int ans=helper(count,tar);
         if(ans==101)return -1;
         return ans;
     }
-    public int helper(int[][] count,int[] tarc,int i){
-        String str=Arrays.toString(tarc)+"_"+i;
-        if(map.containsKey(str)){
-            return map.get(str);
+    public int helper(int[][] count,String tar){
+        if(map.containsKey(tar)){
+            return map.get(tar);
         }
-        if(i>=count.length){
-            boolean valid=true;
+        if(tar.length()==0)return 0;
+        int[] tarc=new int[26];
+      //  System.out.println(tar);
+        for(int i=0;i<tar.length();i++){
+            tarc[tar.charAt(i)-'a']++;
+        }
+        int ans=101;
+        for(int i=0;i<count.length;i++){
+            boolean valid=false;
+            int[] newt=new int[26];
             for(int k=0;k<26;k++){
-                if(tarc[k]>0){
-                    valid=false;
-                    break;
-                }
-            }
-            if(!valid)return 101;
-            return 0;
-        }
-        int val1=helper(count,tarc,i+1);
-        boolean valid=false;
-        int[] newt=new int[26];
-        int val2=101;
-        for(int k=0;k<26;k++){
-            if(tarc[k]>0 && count[i][k]>0){
-                if(tarc[k]>=count[i][k]){
-                    newt[k]=tarc[k]-count[i][k];
+                if(tarc[k]>0 && count[i][k]>0){
+                    if(tarc[k]>=count[i][k]){
+                        newt[k]=tarc[k]-count[i][k];
+                    }else{
+                        newt[k]=0;
+                    }
+                    valid=true;
                 }else{
-                    newt[k]=0;
+                    newt[k]=tarc[k];
                 }
-                valid=true;
-            }else{
-                newt[k]=tarc[k];
+            }
+            if(valid){
+                StringBuilder sb=new StringBuilder();
+                for(int k=0;k<26;k++){
+                    while(newt[k]>0){
+                        sb.append((char)(k+'a'));
+                        newt[k]--;
+                    }
+                }
+                ans=Math.min(ans,helper(count,sb.toString())+1);
             }
         }
-        if(valid){
-            val2=helper(count,newt,i)+1;
-        }
-        map.put(str,Math.min(val1,val2));
-        return Math.min(val1,val2);
+        map.put(tar,ans);
+        return ans;
     }
 }
