b79f858a2b2c213d7867788effdc765b5015d308
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-08-04 14:59:57 +0530

    Time: 109 ms (5.31%) | Memory: 53.7 MB (5.31%) - LeetSync

---
+++ 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int uniqueLetterString(String s) {
+        int n=s.length();
+        int[] next=new int[n];
+        int[] prev=new int[n];
+        int[] ch=new int[26];
+        Arrays.fill(ch,n);
+        for(int i=n-1;i>=0;i--){
+            int c=s.charAt(i)-'A';
+            next[i]=ch[c];
+            ch[c]=i;
+        }
+        ch=new int[26];
+       Arrays.fill(ch,-1);
+        for(int i=0;i<n;i++){
+            int c=s.charAt(i)-'A';
+            prev[i]=ch[c];
+            ch[c]=i;
+        }
+        System.out.println(Arrays.toString(prev));
+        System.out.println(Arrays.toString(next));
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int pr=prev[i];
+            int ne=next[i];
+            ans+=(ne-i)*(i-pr);
+        }
+        return ans;
+    }
+}
