2c560101625a6491fd4eeb86f9df5d6a6aa82c20
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-08-08 22:23:09 +0530

    Time: 304 ms (66.67%) | Memory: 44 MB (95.10%) - LeetSync

---
+++ 1024-triples-with-bitwise-and-equal-to-zero/triples-with-bitwise-and-equal-to-zero.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countTriplets(int[] arr) {
+        int[] map=new int[1<<16];
+        for(int num:arr){
+            for(int num2:arr){
+                map[num&num2]++;
+            }
+        }
+        int ans=0;
+        for(int num:arr){
+            for(int i=0;i<map.length;i++){
+                if((num&i)==0){
+                    ans+=map[i];
+                }
+            }
+        }
+        return ans;
+    }
+}
