a19061f5b63a85a9554c5bdf8d9f98edb36a0493
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-08-08 08:21:42 +0530

    Time: 15 ms (38.92%) | Memory: 163.9 MB (23.35%) - LeetSync

--- 826-soup-servings/soup-servings.java
+++ 826-soup-servings/soup-servings.java
@@ -1,20 +1,15 @@
 class Solution {
-    double[][] memo;
+    Double[][] memo;
     public double soupServings(int n) {
         if(n>=4800)return 1;
-        memo=new double[n+1][n+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                memo[i][j]=-1;
-            }
-        }
+        memo=new Double[n+1][n+1];
         return helper(n,n);
     }
     public double helper(int a,int b){
         if(a==0 && b==0)return 0.5;
         if(a==0)return 1;
         if(b==0)return 0;
-        if(memo[a][b]!=-1)return memo[a][b];
+        if(memo[a][b]!=null)return memo[a][b];
         double val=helper(Math.max(0,a-100),b);
         double val2=helper(Math.max(0,a-75),Math.max(0,b-25));
         double val3=helper(Math.max(0,a-50),Math.max(0,b-50));
