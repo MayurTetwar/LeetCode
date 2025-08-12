f775a90b889fff042cb9065f17a02fb50d081ce6
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-08-12 08:30:59 +0530

    Time: 249 ms (31.05%) | Memory: 44.9 MB (58.95%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.java
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.java
@@ -1,11 +1,8 @@
 class Solution {
-    long[][] memo;
+    Long[][] memo;
     long mod=1000000007L;
     public int numberOfWays(int n, int x) {
-        memo = new long[n+1][n+1];
-        for(int i=0;i<=n;i++){
-            Arrays.fill(memo[i],-1);
-        }
+        memo = new Long[n+1][n+1];
         return (int)helper(n,1,x);
     }
     public long helper(int n,int i,int x){
@@ -13,7 +10,7 @@ class Solution {
         if(n==0)return 1;
         int val=(int)Math.pow(i,x);
         if(val>n)return 0;
-        if(memo[n][i]!=-1) return memo[n][i];
+        if(memo[n][i]!=null) return memo[n][i];
         long res1=helper(n,i+1,x);
         long res2=helper(n-val,i+1,x);
         return memo[n][i]=(res1+res2)%mod;
