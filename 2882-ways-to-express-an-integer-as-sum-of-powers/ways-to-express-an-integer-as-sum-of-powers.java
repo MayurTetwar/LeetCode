9a51d9a80960ed6294dce718d39ad7c3d4adf07c
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-08-12 09:41:54 +0530

    Time: 71 ms (65.79%) | Memory: 45.6 MB (13.16%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.java
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.java
@@ -1,18 +1,18 @@
 class Solution {
-    Long[][] memo;
-    long mod=1000000007L;
+    int[][] dp;
+    int mod=1000000007;
     public int numberOfWays(int n, int x) {
-        memo = new Long[n+1][n+1];
-        return (int)helper(n,1,x);
-    }
-    public long helper(int n,int i,int x){
-        if(n<0)return 0;
-        if(n==0)return 1;
-        int val=(int)Math.pow(i,x);
-        if(val>n)return 0;
-        if(memo[n][i]!=null) return memo[n][i];
-        long res1=helper(n,i+1,x);
-        long res2=helper(n-val,i+1,x);
-        return memo[n][i]=(res1+res2)%mod;
+        dp = new int[n+1][n+1];
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+            int pow=(int)Math.pow(i,x);
+            for(int j=0;j<=n;j++){
+                dp[i][j]=dp[i-1][j];
+                if(j>=pow){
+                    dp[i][j]=(dp[i][j]+dp[i-1][j-pow])%mod;
+                }
+            }
+        }
+        return dp[n][n];
     }
 }
