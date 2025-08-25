66c82b5af19dd06bf94314d8b696ec1845d7d9d8
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-08-25 19:34:27 +0530

    Time: 27 ms (62.50%) | Memory: 45.4 MB (9.95%) - LeetSync

---
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxPoints(int[][] arr) {
+        int n=arr.length;
+        int ans=1;
+        for(int i=0;i<n;i++){
+            HashMap<Double,Integer> map=new HashMap<>();
+            for(int j=0;j<n;j++){
+                if(i==j)continue;
+                double num=arr[i][0]-arr[j][0];
+                double deno=arr[i][1]-arr[j][1];
+                double val=0;
+                if(num==0){
+                    val=50001;
+                }else if(deno==0){
+                    val=-50001;
+                }else{
+                    val=num/deno;
+                }
+                if(map.containsKey(val)){
+                    map.put(val,map.get(val)+1);
+                }else{
+                    map.put(val,2);
+                }
+                ans=Math.max(ans,map.get(val));
+            }
+        }
+        return ans;
+    }
+}
