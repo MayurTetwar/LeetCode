fab604b3419db835afbc35bbf736421aecdd297a
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-08-25 19:43:08 +0530

    Time: 20 ms (82.40%) | Memory: 44.8 MB (54.23%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.java
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -4,8 +4,7 @@ class Solution {
         int ans=1;
         for(int i=0;i<n;i++){
             HashMap<Double,Integer> map=new HashMap<>();
-            for(int j=0;j<n;j++){
-                if(i==j)continue;
+            for(int j=i+1;j<n;j++){
                 double num=arr[i][0]-arr[j][0];
                 double deno=arr[i][1]-arr[j][1];
                 double val=0;
