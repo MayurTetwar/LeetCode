bfbd7f0ccd49d8ba608040786aedff0fa483b1bc
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-10-09 10:33:37 +0530

    Time: 135 ms (54.93%) | Memory: 44.9 MB (47.89%) - LeetSync

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
@@ -3,38 +3,16 @@ class Solution {
         int n=arr.length;
         int m=mana.length;
         long[] dp=new long[n];
-        dp[0]=arr[0]*mana[0];
-        for(int i=1;i<n;i++){
-            dp[i]=dp[i-1]+arr[i]*mana[0];
-        }
-        for(int i=1;i<m;i++){
-            // System.out.println(Arrays.toString(dp));
-            long st=dp[0];
-            long end=dp[n-1];
-            while(st<=end){
-                long mid=st+(end-st)/2;
-                if(check(dp,arr,mana[i],mid)){
-                    end=mid-1;
-                }else{
-                    st=mid+1;
-                }
-                // System.out.println(st+" "+end);
+        for(int i=0;i<m;i++){
+            long curr=0;
+            for(int j=0;j<n;j++){
+               curr=Math.max(curr,dp[j])+arr[j]*mana[i];
             }
-            long corr=end+1;
-            dp[0]=corr+arr[0]*mana[i];
-            for(int j=1;j<n;j++){
-                dp[j]=dp[j-1]+arr[j]*mana[i];
+            dp[n-1]=curr;
+            for(int j=n-2;j>=0;j--){
+                dp[j]=dp[j+1]-arr[j+1]*mana[i];
             }
         }
         return dp[n-1];
-    }
-    public boolean check(long[] dp,int[] arr,int m,long corr){
-        for(int i=0;i<arr.length-1;i++){
-            if((corr+arr[i]*m)<dp[i+1]){
-                return false;
-            }
-            corr+=arr[i]*m;
-        }
-        return true;
     }
 }
