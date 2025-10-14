550cb1b218ffd58e5d8e9924c3e27f3b95fd1ddf
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-10-15 00:47:09 +0530

    Time: 578 ms (36.28%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,7 +1,4 @@
--- Write your PostgreSQL query statement below
-select 
-p.project_id,round(avg(e.experience_years),2) as average_years 
-from Project p left join Employee e
-on e.employee_id = p.employee_id 
+/* Write your PL/SQL query statement below */
+SELECT p.project_id ,round(avg(experience_years),2) as average_years  
+FROM Project p natural join Employee e
 group by p.project_id;
-
