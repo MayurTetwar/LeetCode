b561b6300e3c15d2e4a882b1a0fbe4b21c0a03af
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-10-15 00:34:14 +0530

    Time: 705 ms (18.64%) | Memory: 0B (100.00%) - LeetSync

--- 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -1,2 +1,4 @@
--- Write your PostgreSQL query statement below
-select product_id from Products where low_fats='Y' and recyclable='Y';
+/* Write your PL/SQL query statement below */
+SELECT DISTINCT product_id 
+from Products
+where low_fats='Y' and recyclable='Y';
