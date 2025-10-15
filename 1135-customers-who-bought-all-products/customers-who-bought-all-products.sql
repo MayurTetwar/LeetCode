d6f7dac64bed5d0172d2b19c279c7421b596e3b1
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-10-15 07:59:52 +0530

    Time: 601 ms (40.34%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,7 +1,7 @@
--- Write your PostgreSQL query statement below
-select customer_id
-from  Customer 
-group by customer_id 
-having count(distinct product_key)=(
-    select count(*) from Product 
+/* Write your PL/SQL query statement below */
+SELECT customer_id 
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key)=(
+    SELECT COUNT(*) FROM Product
 )
