c2598d0268a2bab47a1a0fc7e6aca2db87832e01
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-10-15 07:44:55 +0530

    Time: 436 ms (89.39%) | Memory: 0B (100.00%) - LeetSync

---
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -0,0 +1,11 @@
+/* Write your PL/SQL query statement below */
+select 
+(case
+    when count(*)=0 then null
+    else max(num)
+end)as num
+from 
+(select num 
+from MyNumbers
+group by num 
+having count(num)=1);
