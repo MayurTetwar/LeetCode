ec2045bd114cd40579dbfccff1d4275fbdd90c3b
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-10-15 07:57:02 +0530

    Time: 472 ms (83.83%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,11 +1,9 @@
--- Write your PostgreSQL query statement below
-select product_id,year as first_year
-,quantity,price
-from Sales 
+/* Write your PL/SQL query statement below */
+SELECT 
+product_id,year as first_year,quantity,price 
+FROM Sales 
 where (product_id,year) in (
-   select product_id,min(year) from Sales
-   group by product_id 
-);
- 
-
- 
+    SELECT product_id,MIN(YEAR) 
+    FROM Sales
+    GROUP BY product_id
+)
