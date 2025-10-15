a7ef620103151576f51afd602be0b3e16643c3cb
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-10-15 07:17:59 +0530

    Time: 488 ms (86.60%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,21 +1,18 @@
--- Write your PostgreSQL query statement below
-select 
-TO_CHAR(trans_date,'YYYY-MM') as month,
-country,
-count(state) as trans_count,
-sum(
-    case
-        when state='approved' then 1
-        else 0
-    end
-) as approved_count,
-sum(amount) as trans_total_amount,
-sum(
-    case
-        when state='approved' then amount
-        else 0
-    end
-) as approved_total_amount 
-from 
-Transactions 
-group by country,TO_CHAR(trans_date,'YYYY-MM');
+/* Write your PL/SQL query statement below */
+SELECT 
+    TO_CHAR(trans_date,'YYYY-MM') as month,country,
+    count(*) as trans_count,
+    count(
+        case 
+            when state='approved' then 1
+        end
+    )as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(
+        case 
+            when state='approved' then amount
+            else 0
+        end
+    )as approved_total_amount
+from Transactions
+group by TO_CHAR(trans_date,'YYYY-MM'),country;
