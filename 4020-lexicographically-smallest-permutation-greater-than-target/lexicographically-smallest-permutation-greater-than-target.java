f7a03d3a8a241edc2cadd4c682de70d74893d8b6
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-10-19 17:24:12 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (0.00%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
@@ -15,45 +15,24 @@ class Solution {
                 break;
             }
         }
-            //System.out.println(mark);
-            // System.out.println(Arrays.toString(ans)+" "+ind);
-            // System.out.println(Arrays.toString(cnt));
-        if(mark==-1){
-            //System.out.println(Arrays.toString(ans)+" "+ind);
-            boolean flag=false;
-            int ki=-1;
-            for(int i=n-1;i>=0;i--){
-                for(int j=0;j<26;j++){
-                    if((char)(j+'a')>ans[i] && cnt[j]>0){
-                        ind=j;
-                        ki=i;
-                        flag=true;
-                        break;
-                    }
-                }
-                if(flag)break;
-                cnt[ans[i]-'a']++;
-            }
-            if(!flag)return "";
-                // System.out.println(Arrays.toString(ans)+" "+ki+" "+ind);
-            cnt[ans[ki]-'a']++;
-            ans[ki]=(char)(ind+'a');
-            cnt[ind]--;
-            ind=ki+1;
-        }else{
+        boolean req=true;
+        if(mark!=-1){
             for(int i=0;i<26;i++){
                 if((char)(i+'a')>tar.charAt(mark) && cnt[i]>0){
                     ind=i;
                     break;
                 }
             }
-
-            // System.out.println(Arrays.toString(cnt)+" "+ind);
             if(ind!=-1){
                 cnt[ind]--;
                 ans[mark]=(char)(ind+'a');
                 ind=mark+1;
+                req=false;
             }else{
+                req=true;
+            }
+        }else mark=n;
+        if(req){
                 boolean flag=false;
                 int ki=-1;
                 for(int i=mark-1;i>=0;i--){
@@ -69,20 +48,18 @@ class Solution {
                     cnt[ans[i]-'a']++;
                 }
                 if(!flag)return "";
-                    // System.out.println(Arrays.toString(ans)+" "+ki+" "+ind);
                 cnt[ans[ki]-'a']++;
                 ans[ki]=(char)(ind+'a');
                 cnt[ind]--;
                 ind=ki+1;
+        }
+        for(int i=0;i<26;i++){
+            while(cnt[i]>0){
+                ans[ind++]=(char)(i+'a');
+                cnt[i]--;
             }
         }
-          for(int i=0;i<26;i++){
-                while(cnt[i]>0){
-                    ans[ind++]=(char)(i+'a');
-                    cnt[i]--;
-                }
-            }
-            return new String(ans);
+        return new String(ans);
     }
 }
 /*
