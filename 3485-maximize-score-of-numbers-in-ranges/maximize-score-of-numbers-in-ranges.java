e183bdf5e7bda1a63dd36c840cfc0ebd9054b6a7
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-10-28 00:29:37 +0530

    Time: 55 ms (6.93%) | Memory: 63.4 MB (40.59%) - LeetSync

---
+++ 3485-maximize-score-of-numbers-in-ranges/maximize-score-of-numbers-in-ranges.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxPossibleScore(int[] arr, int d) {
+        Arrays.sort(arr);
+        int n=arr.length;
+        int st=0;
+        int end=arr[n-1]+d;
+        int ans=1000000000;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(helper(arr.clone(),d,mid)){
+                st=mid+1;
+                ans=mid;
+            }else{
+                end=mid-1;
+            }
+        //    System.out.println(ans+" "+st+" "+end);
+        }
+        return Math.min(arr[n-1]+d-arr[0],ans);
+    }
+    public boolean helper(int[] arr,int d,int tar){
+        int n=arr.length;
+        long next=arr[0]+tar;
+    //    if(next>arr[0]+d)return false;
+        for(int i=1;i<n;i++){
+            if(arr[i]+d<next)return false;
+            next=Math.max(next,arr[i])+tar;
+        }
+        // System.out.println(tar+" "+next);
+        return true;
+    }
+    // 4,10,14,15,19
+    // 5,10,15,20,25
+}
