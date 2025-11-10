7647d3ea6e37aa770def8fd9d062025ec1c14984
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2025-11-10 08:43:41 +0530

    Time: 145 ms (38.03%) | Memory: 123.8 MB (9.16%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
@@ -1,42 +1,15 @@
 class Solution {
     public int minOperations(int[] arr) {
+        int n=arr.length;
         Stack<Integer> stk=new Stack<>();
         int ans=0;
-        int n=arr.length;
-        HashSet<Integer> set=new HashSet<>();
         for(int i=0;i<n;i++){
-            set=new HashSet<>();
-            if(arr[i]==0){
-                while(stk.size()>0){
-                    if(!set.contains(arr[stk.peek()])){
-                        ans++;
-                        // System.out.println(i);
-                        set.add(arr[stk.peek()]);
-                    }
-                    // set.remove(arr[stk.pop()]);
-                    stk.pop();
-                } 
-                continue;
-            }
-            while(stk.size()>0 && arr[stk.peek()]>arr[i]){
-                if(!set.contains(arr[stk.peek()])){
-                    ans++;
-                    // System.out.println(i);
-                    set.add(arr[stk.peek()]);
-                }
-                // set.remove(arr[stk.pop()]);
+            while(!stk.isEmpty() && stk.peek()>=arr[i]){
+                if(stk.peek()>arr[i])ans++;
                 stk.pop();
-            } 
-            stk.push(i);
-            // System.out.println(stk);
-        }
-        while(stk.size()>0){
-            if(!set.contains(arr[stk.peek()])){
-                ans++;
-                set.add(arr[stk.peek()]);
             }
-            stk.pop();
-        } 
-        return ans;
+            if(arr[i]>0)stk.push(arr[i]);
+        }
+        return stk.size()+ans;
     }
 }
