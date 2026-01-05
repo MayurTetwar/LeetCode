31b4434a9f7d4c40143e6a614217cd63b44d9d93
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-01-05 17:48:26 +0530

    Time: 6 ms (73.94%) | Memory: 68.1 MB (52.73%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.java
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -2,20 +2,15 @@ class Solution {
     public long maxMatrixSum(int[][] arr) {
         long sum=0;
         int count=0;
-        int max=-1000001;
         int min=1000001;
         for(int[] ar:arr){
             for(int a:ar){
-                if(a<=0){
-                    max=Math.max(max,a);
-                    count++;
-                }else{
-                    min=Math.min(min,a);
-                }
+                if(a<0)count++;
+                min=Math.min(min,Math.abs(a));
                 sum+=Math.abs(a);
             }
         }
-        if(count%2==1)return Math.max(sum+max*2,sum-min*2);
+        if(count%2==1)return sum-min*2;
         return sum;
     }
 }
