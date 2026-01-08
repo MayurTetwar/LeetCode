ff8b7ea8b3b681847580f4da42095790b7a63990
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-01-08 15:39:53 +0530

    Time: 11 ms (59.26%) | Memory: 48.8 MB (9.63%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
@@ -1,23 +1,22 @@
 class Solution {
     int m;
     int n;
-    Integer[][][] memo;
+    Integer[][] memo;
     public int maxDotProduct(int[] arr1, int[] arr2) {
         m=arr2.length;
         n=arr1.length;
-        memo=new Integer[n][m][2];
-        return helper(arr1,arr2,0,0,0);
+        memo=new Integer[n][m];
+        return helper(arr1,arr2,0,0);
     }
-    public int helper(int[] arr1,int[] arr2,int i,int j,int take){
+    public int helper(int[] arr1,int[] arr2,int i,int j){
         if(i==n || j==m){
-            if(take==1)return 0;
             return Integer.MIN_VALUE;
         }
-        if(memo[i][j][take]!=null)return memo[i][j][take];
+        if(memo[i][j]!=null)return memo[i][j];
         int val=Integer.MIN_VALUE;
-        val=Math.max(val,helper(arr1,arr2,i+1,j,take));
-        val=Math.max(val,helper(arr1,arr2,i,j+1,take));
-        val=Math.max(val,helper(arr1,arr2,i+1,j+1,1)+arr1[i]*arr2[j]);
-        return memo[i][j][take]=val;
+        val=Math.max(val,helper(arr1,arr2,i+1,j));
+        val=Math.max(val,helper(arr1,arr2,i,j+1));
+        val=Math.max(val,Math.max(0,helper(arr1,arr2,i+1,j+1))+arr1[i]*arr2[j]);
+        return memo[i][j]=val;
     }
 }
