c6bdc6e7a7c492559c074cb4b3d0616db40978d6
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-01-13 15:26:03 +0530

    Time: 136 ms (39.47%) | Memory: 149 MB (38.42%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.java
+++ 3763-separate-squares-i/separate-squares-i.java
@@ -1,32 +1,34 @@
 class Solution {
     public double separateSquares(int[][] arr) {
         double st=0;
+        double end=1000000000001.0;
+        double mid=-1;
         double eps = 1e-5;
-        double total=0;
-        double end=0;
-        for(int[] a:arr){
-            total+=(double)a[2]*a[2];
-            end=Math.max(end,(double)a[1]+a[2]);
-        }
         while(Math.abs(end-st)>eps){
-            double mid=(end+st)/2.0;
-            boolean ans=helper(arr,mid,total);
-            if(!ans){
+            mid=((end-st)/2.0)+st;
+            double ans=helper(arr,mid);
+            if(ans<0){
                 st=mid;
             }else{
                 end=mid;
             }
         }
-        return end;
+        return st;
     }
-    public boolean helper(int[][] arr,double mid,double total){
-        double area=0;
+    public double helper(int[][] arr,double mid){
+        double lower =0;
+        double upper =0;
         for(int[] a:arr){
-            if(a[1]<mid){
-                area+=(double)a[2]*Math.min(mid-a[1],(double)a[2]);
+            if(a[1]<=mid && mid<=a[1]+a[2]){
+                lower+=(double)(a[1]+a[2]-mid)*a[2];
+                upper+=(double)(mid-a[1])*a[2];
+            }else if(mid<=a[1]){
+                lower+=(double)a[2]*a[2];
+            }else if(mid>=a[1]+a[2]){
+                upper+=(double)a[2]*a[2];
             }
         }
         // System.out.println(upper+" "+lower);
-        return area>=total/2;
+        return upper-lower;
     }
 }
