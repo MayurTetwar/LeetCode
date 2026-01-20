934ac07f281aec8828e0ffd6488d9fcc8700511e
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-01-20 07:49:58 +0530

    Time: 3 ms (57.02%) | Memory: 46.7 MB (65.79%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
@@ -1,21 +1,14 @@
 class Solution {
     public int[] minBitwiseArray(List<Integer> list) {
-        int[] cash=new int[1001];
         int n=list.size();
         int[] ans=new int[n];
         Arrays.fill(ans,-1);
-        cash[2]=-1;
         for(int i=0;i<n;i++){
             int num=list.get(i);
-            if(cash[num]!=0){
-                ans[i]=cash[num];
-                continue;
-            }
             int val=1;
             while(val<num){
                 if((val|(val+1))==num){
                     ans[i]=val;
-                    cash[num]=val;
                     break;
                 }
                 val++;
