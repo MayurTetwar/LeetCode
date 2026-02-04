01d01421a3f59c195acb002547ae40ac9b2ecf58
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-02-04 17:56:34 +0530

    Time: 3 ms (94.87%) | Memory: 95.1 MB (74.36%) - LeetSync

---
+++ 3956-trionic-array-ii/trionic-array-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public long maxSumTrionic(int[] arr) {
+        int n=arr.length;
+        
+        int i=0;
+        while(i<n-1 && arr[i]>=arr[i+1]){
+            i++;
+        }
+        while(i<n-1 && arr[i]<arr[i+1])i++;
+        long ans=Long.MIN_VALUE;
+
+        while(i<n-1){
+            // System.out.println(i);
+            long prev=arr[i]+arr[i-1];
+            for(int p=i-1;p>0 && arr[p-1]<arr[p];p--){
+                prev=Math.max(prev,prev+arr[p-1]);
+            }
+            
+            if(i<n-1 && arr[i]==arr[i+1]){
+                while(i<n-1 && arr[i]>=arr[i+1])i++;
+                while(i<n-1 && arr[i]<arr[i+1])i++;
+                continue;
+            }
+            long curr=0;
+            while(i<n-1 && arr[i]>arr[i+1]){
+                curr+=arr[i+1];
+                i++;
+            }
+            if(i<n-1 && arr[i]==arr[i+1]){
+                while(i<n-1 && arr[i]>=arr[i+1])i++;
+                while(i<n-1 && arr[i]<arr[i+1])i++;
+                continue;
+            }
+            if(i==n-1)break;
+            long next=0;
+            // System.out.println(i);
+            int st=i+1;
+            while(i<n-1 && arr[i]<arr[i+1]){
+                next+=arr[i+1];
+                i++;
+            }
+            // if(st<n)System.out.println(prev+" "+curr+" "+Math.max(next,arr[st])+" "+i);
+            if(st<n)ans=Math.max(ans,prev+curr+Math.max(next,arr[st]));
+        }
+        return ans;
+    }
+}
+// [0 > -2 < -1 > -3 < 0 < 2 > -1]
