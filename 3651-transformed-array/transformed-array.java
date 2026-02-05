869dd71acbc2da40f46b45881824dfcebc24bb95
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-02-05 23:48:37 +0530

    Time: 1 ms (98.72%) | Memory: 47.1 MB (13.78%) - LeetSync

--- 3651-transformed-array/transformed-array.java
+++ 3651-transformed-array/transformed-array.java
@@ -3,17 +3,7 @@ class Solution {
         int n=arr.length;
         int[] ans=new int[n];
         for(int i=0;i<n;i++){
-            if(arr[i]>0){
-                int ind=(i+arr[i])%n;
-                ans[i]=arr[ind];
-            }else if(arr[i]<0){
-                int ind=(i+arr[i])%n;
-                if(ind<0)ind+=n;
-                // System.out.println(ind);
-                ans[i]=arr[ind];
-            }else{
-                ans[i]=arr[i];
-            }
+            ans[i]=arr[((((i+arr[i])%n)+n)%n)];
         }
         return ans;
     }
