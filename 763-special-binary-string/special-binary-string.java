5372a08e979abd240188a76e69dbcffe4113100d
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-02-20 22:19:06 +0530

    Time: 4 ms (15.40%) | Memory: 43.5 MB (19.67%) - LeetSync

--- 763-special-binary-string/special-binary-string.java
+++ 763-special-binary-string/special-binary-string.java
@@ -1,18 +1,19 @@
 class Solution {
     public String makeLargestSpecial(String s) {
-        List<String> list=new ArrayList<>();
-        int i=0;
-        int count=0;
-        int n=s.length();
-        for(int j=0;j<n;j++){
-            if(s.charAt(j)=='1')count++;
+        int count = 0, i = 0;
+        List<String> res = new ArrayList<>();
+        
+        for (int j = 0; j < s.length(); j++) {
+            if (s.charAt(j) == '1') count++;
             else count--;
-            if(count==0){
-                list.add("1".concat(makeLargestSpecial(s.substring(i+1,j))).concat("0"));
-                i=j+1;
-            }          
+            
+            if (count == 0) {
+                res.add('1' + makeLargestSpecial(s.substring(i + 1, j)) + '0');
+                i = j + 1; 
+            }
         }
-        Collections.sort(list,Collections.reverseOrder());
-        return String.join("",list);
+        
+        Collections.sort(res, Collections.reverseOrder());
+        return String.join("", res);
     }
 }
