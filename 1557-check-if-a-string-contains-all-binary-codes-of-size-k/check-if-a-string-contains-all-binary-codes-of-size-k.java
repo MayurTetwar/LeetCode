eac1789921bcfe2fc770358beca863eb58034189
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-02-23 09:10:27 +0530

    Time: 76 ms (83.70%) | Memory: 73.1 MB (80.25%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -2,9 +2,23 @@ class Solution {
     // int decimal = Integer.parseInt(binaryString, 2);
     public boolean hasAllCodes(String s, int k) {
         HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i+k<=s.length();i++){
-            set.add(Integer.parseInt(s.substring(i,i+k),2));
+        int val=0;
+        for(int i=0;i<k && i<s.length();i++){
+            val=val<<1;
+            if(s.charAt(i)=='1')
+                val=val|1;
         }
+        for(int i=k;i<s.length();i++){
+            set.add(val);
+            if(s.charAt(i-k)=='1'){
+                val=val&(~(1<<(k-1)));
+            }
+            val=val<<1;
+            if(s.charAt(i)=='1'){
+                val=val|1;
+            }
+        }
+        set.add(val);
         return set.size()==(1<<k);
     }
 }
