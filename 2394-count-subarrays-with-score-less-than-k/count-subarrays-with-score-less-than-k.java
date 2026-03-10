749cfdff8e98b20f2a9bf3de4fc9a3d7b2a29efc
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-03-10 09:39:13 +0530

    Time: 3 ms (42.90%) | Memory: 88.3 MB (34.45%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.java
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.java
@@ -1,39 +1,17 @@
 class Solution {
-    static{
-        for(int i=0;i<500;i++){
-            countSubarrays(new int[]{1},0);
-        }
-    }
-    public static long countSubarrays(int[] arr, long k) {
-        int n = arr.length;
-        long prefix[] = new long[n];
-        prefix[0] = arr[0];
-        for (int i = 1; i < n; i++) {
-            prefix[i] = arr[i] + prefix[i - 1];
-        }
-        long ans = 0;
-        long pre = 0;
-        int i = 0;
-        int ind = 0;
-        for (i = 0; i < n; i++) {
-            ind = helper(prefix, k, pre, i) - i;
-            ans += ind;
-            pre = prefix[i];
+    public long countSubarrays(int[] arr, long k) {
+        long ans=0;
+        int n=arr.length;
+        int j=0;
+        long sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            while(j<i && sum*(i-j+1)>=k){
+                sum-=arr[j];
+                j++;
+            }
+            if(sum*(i-j+1)<k)ans+=(i-j+1);
         }
         return ans;
-    }
-
-    public static int helper(long[] arr, long k, long pre, int i) {
-        int st = i;
-        int end = arr.length;
-        while (st < end) {
-            int mid = (end + st) / 2;
-            if (((arr[mid] - pre) * (mid - i + 1)) < k) {
-                st = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-        return end;
     }
 }
