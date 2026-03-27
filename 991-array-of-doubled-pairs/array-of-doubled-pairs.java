87d7139cce0f34826f8cb07ff23b7ff3ee3e74b8
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-03-27 08:33:34 +0530

    Time: 97 ms (22.52%) | Memory: 54.6 MB (40.08%) - LeetSync

--- 991-array-of-doubled-pairs/array-of-doubled-pairs.java
+++ 991-array-of-doubled-pairs/array-of-doubled-pairs.java
@@ -3,13 +3,7 @@ class Solution {
         int n=arr.length;
         HashMap<Integer,Integer> map=new HashMap<>();
         PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->{
-            if(a>0 && b>0){
-                return a-b;
-            }else if(a<0 && b<0){
-                return b-a;
-            }else{
-                return a-b;
-            }
+            return Math.abs(a)-Math.abs(b);
         });
         for(int i=0;i<n;i++){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
@@ -22,23 +16,14 @@ class Solution {
             if(set.contains(num))continue;
             set.add(num);
             int c1=map.get(num);
-            if(num==0){
-                if(c1%2==0){
-                    count+=c1/2;
-                }
-                continue;
-            }
             if(c1==0)continue;
             if(!map.containsKey(num*2)){
                 return false;
             }
             int c2=map.get(num*2);
-            // System.out.println(num+" "+set+","+c1+" "+c2);
             if(c1>c2)return false;
-            int rem=c2-c1;
-            count+=c1;
-            map.put(num*2,rem);
+            map.put(num*2,c2-c1);
         }
-        return count==n/2;
+        return true;
     }
 }
