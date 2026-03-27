a59cfd4817dfc3d57bba21a9eb7b994b5dbe3495
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-03-27 22:29:04 +0530

    Time: 4 ms (89.78%) | Memory: 45.8 MB (96.91%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,22 +1,19 @@
 class Solution {
-    Integer[] memo;
     public int lengthOfLIS(int[] arr) {
         int n=arr.length;
-        memo=new Integer[n];
-        return helper(arr,-1);
-    }
-    public int helper(int[] arr,int i){
-        int n=arr.length;
-        if(i==n)return 0; 
-        if(i!=-1 && memo[i]!=null)return memo[i];
-        int res=0;
-        for(int j=i+1;j<n;j++){
-            if(i==-1){
-                res=Math.max(res,helper(arr,j)+1);
-            }else if(arr[i]<arr[j])
-                res=Math.max(res,helper(arr,j)+1);
+        int[] dp=new int[n];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int idx=Arrays.binarySearch(dp,arr[i]);
+            if(idx<0){
+                idx=-idx-1;
+            }
+            ans=Math.max(ans,idx+1);
+            if(dp[idx]>arr[i]){
+                dp[idx]=arr[i];
+            }
         }
-        if(i!=-1)memo[i]=res;
-        return res;
+        return ans;
     }
 }
