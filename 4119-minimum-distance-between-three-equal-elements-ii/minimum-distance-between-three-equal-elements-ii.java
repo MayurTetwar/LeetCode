9cf8df3e5d81427c7aa5c6b14c876b77a14d90ec
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-04-11 15:33:50 +0530

    Time: 129 ms (43.57%) | Memory: 274.6 MB (42.46%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.java
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.java
@@ -1,19 +1,22 @@
 class Solution {
     public int minimumDistance(int[] arr) {
         int n=arr.length;
+        int ans=n*3;
         HashMap<Integer,Integer> map=new HashMap<>();
-        int[] right=new int[n];
-        for(int i=n-1;i>=0;i--){
-            right[i]=map.getOrDefault(arr[i],-1);
+        int[] prev=new int[n];
+        for(int i=0;i<n;i++){
+            int pre=map.getOrDefault(arr[i],-1);
+            prev[i]=pre;
             map.put(arr[i],i);
         }
-        int ans=3*n;
         map=new HashMap<>();
-        map.put(arr[0],0);
-        for(int i=1;i<n;i++){
-            if(map.containsKey(arr[i]) && right[i]!=-1){
-                int k=map.get(arr[i]);
-                ans=Math.min(ans,Math.abs(k - i) + Math.abs(i - right[i]) + Math.abs(right[i] - k));
+        for(int i=0;i<n;i++){
+            int pre=map.getOrDefault(arr[i],-1);
+            if(pre!=-1){
+                int pre2=prev[pre];
+                if(pre2!=-1){
+                    ans=Math.min(ans,Math.abs(i-pre)+Math.abs(pre-pre2)+Math.abs(i-pre2));
+                }
             }
             map.put(arr[i],i);
         }
