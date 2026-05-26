d733e19deacd8c79e47a00f965bf868022552031
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-05-26 08:11:08 +0530

    Time: 13 ms (39.37%) | Memory: 47 MB (99.83%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.java
+++ 2001-jump-game-vii/jump-game-vii.java
@@ -8,21 +8,12 @@ class Solution {
             if(s.charAt(i)=='1')continue;
             int end=Math.min(n-1,i+maxJump);
             right=Math.max(right,i+minJump);
-            // System.out.println(i+" "+visi[i]+" "+end+" "+right);
             while(right<=end){
-                // if(i==9){
-                //     System.out.println(right+" "+(right==n-1)+" "+visi[i]);
-                // }
                 if(s.charAt(right)=='0')visi[right]=visi[right]|visi[i];
-                // if(i==9){
-                    // System.out.println(Arrays.toString(visi));
-                // }
                 right++;
             }
             right--;
-            if(visi[n-1])return true;
         }
-        // System.out.println(Arrays.toString(visi));
-        return false;
+        return visi[n-1];
     }
 }
