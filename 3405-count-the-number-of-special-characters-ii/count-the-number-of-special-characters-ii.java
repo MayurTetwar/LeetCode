a9f9d181c0d9c53e41945e59b0380450237c84c0
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-05-27 11:24:57 +0530

    Time: 9 ms (80.58%) | Memory: 48.4 MB (22.31%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -1,26 +1,33 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        int[] visi=new int[26];
-        int[] low=new int[26];
-        // Arrays.fill(visi,true);
-        for(char ch:word.toCharArray()){
-            if(ch>='A' && ch<='Z'){
-                if(visi[ch-'A']==0){
-                    if(low[ch-'A']>0)visi[ch-'A']++;
-                    else visi[ch-'A']=-(int)1e9;
-                }
-            }else{
-                if(visi[ch-'a']>0){
-                    visi[ch-'a']=-(int)1e9;
-                }else{
-                    low[ch-'a']++;
+
+        int lowercase[]=new int[26];
+        int uppercase[]=new int[26];
+        Arrays.fill(lowercase,-1);
+        Arrays.fill(uppercase,-1);
+        int count=0;
+        for(int i=0;i<word.length();i++)
+        {
+            char ch=word.charAt(i);
+            if(ch>='a' && ch<='z')
+            {
+                lowercase[ch-'a']=i;
+            }
+            else{
+                int index= ch-'A';
+                if(uppercase[index]==-1)
+                {
+                    uppercase[index]=i;
                 }
             }
+        }  
+        for(int i=0;i<26;i++)
+        {
+            if(lowercase[i]!=-1 && uppercase[i]!=-1 && lowercase[i]<uppercase[i])
+            {
+                count++;
+            }
         }
-        int ans=0;
-        for(int i=0;i<26;i++){
-            if(visi[i]>0)ans++;
-        }
-        return ans;
+        return count;
     }
 }
