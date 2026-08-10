3ce33d5ac3e17f3cbb1f1c465a20747bc7991255
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-08-10 18:58:08 +0530

    Time: 1438 ms (6.19%) | Memory: 124.1 MB (5.31%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int stoneGameVIII(int[] arr) {
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            arr[i]+=arr[i-1];
+        }
+        return helper(arr,1);
+    }
+    public int helper(int[] arr,int i){
+        int n=arr.length;
+        if(i==n-1)return arr[n-1];
+        int res1=helper(arr,i+1);
+        int res2=arr[i]-res1;
+        System.out.println(res1+" "+res2+" "+i);
+        return Math.max(res1,res2);
+    }
+}
