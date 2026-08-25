c6bd3a3159e64ca8b54202f6ff60abfb35a42422
Mayur Tetwar <163837852+MayurTetwar@users.noreply.github.com>
2026-08-25 14:07:00 +0530

    Time: 6 ms (100.00%) | Memory: 44.7 MB (20.09%) - LeetSync

--- 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.java
+++ 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.java
@@ -1,35 +1,33 @@
 class Solution {
     int[] memo;
     public int maxHeight(int[][] arr) {
-        int[][][] map=new int[101][101][101];
-        memo=new int[arr.length+1];
+        int n=arr.length;
+        memo=new int[n+1];
+        int[][] brr=new int[n+1][3];
         Arrays.fill(memo,-1);
+        int i=1;
+        brr[0]=new int[]{0,0,0};
         for(int[] a:arr){
             Arrays.sort(a);
-            map[a[0]][a[1]][a[2]]++;
+            brr[i]=a;
+            i++;
         }
-        List<int[]> list=new ArrayList<>();
-        list.add(new int[]{0,0,0,0});
-        for(int i=1;i<=100;i++){
-            for(int j=1;j<=100;j++){
-                for(int k=1;k<=100;k++){
-                    if(map[i][j][k]>0)
-                        list.add(new int[]{i,j,k,map[i][j][k]});
-                }
-            }
-        }
-        return helper(list,0);
+        Arrays.sort(brr,(a,b)->{
+            if(a[0]!=b[0])return a[0]-b[0];
+            if(a[1]!=b[1])return a[1]-b[1];
+            return a[2]-b[2];
+        });
+        return helper(brr,0);
     }
-    public int helper(List<int[]> list,int i){
+    public int helper(int[][] arr,int i){
+        int n=arr.length;
         if(memo[i]!=-1)return memo[i];
         int ans=0;
-        int n=list.size();
-        int[] up=list.get(i);
-        for(int j=0;j<n;j++){
-            if(i==j)continue;
-            int[] down=list.get(j);
+        int[] up=arr[i];
+        for(int j=i+1;j<n;j++){
+            int[] down=arr[j];  
             if(up[0]<=down[0] && up[1]<=down[1] && up[2]<=down[2]){
-                ans=Math.max(ans,helper(list,j)+down[3]*down[2]);
+                ans=Math.max(ans,helper(arr,j)+down[2]);
             }
         }
         return memo[i]=ans;
